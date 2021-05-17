33c1baf99e9c274ce8ba395cfc75279ada346e6e
ilariacriv <58052642+ilariacriv@users.noreply.github.com>
2021-05-17 22:52:31 +0200

    Update Main.java

--- hanabi-dataset/src/Main.java
+++ hanabi-dataset/src/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
     public static void main(String args[]){
 
-        ReadFile readfile= new ReadFile("gamefile.txt", "actionfile.txt");
+        ReadFile readfile= new ReadFile("./partite_hanabi/game_0000000.txt", "./azioni_hanabi/actions_0000000.txt");
         RawState s= readfile.readState();
         RawAction a =readfile.readAction();
 
         System.out.println("letto");
-        
+
         readfile.close();
 
     }
