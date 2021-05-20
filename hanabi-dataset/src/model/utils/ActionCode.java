bf7f852f87842a49bef2edccc655416a7e16f993
vareight <63202534+vareight@users.noreply.github.com>
2021-05-20 22:07:00 +0200

    add action code

---
+++ hanabi-dataset/src/model/utils/ActionCode.java
@@ -0,0 +1,27 @@
+package model.utils;
+
+public enum ActionCode {
+    PLAY_1st,
+    PLAY_2nd,
+    PLAY_3rd,
+    PLAY_4th,
+    PLAY_5th,
+
+    DISCARD_1st,
+    DISCARD_2nd,
+    DISCARD_3rd,
+    DISCARD_4th,
+    DISCARD_5th,
+
+    HINT_VALUE_1,
+    HINT_VALUE_2,
+    HINT_VALUE_3,
+    HINT_VALUE_4,
+    HINT_VALUE_5,
+
+    HINT_WHITE,
+    HINT_BLUE,
+    HINT_RED,
+    HINT_YELLOW,
+    HINT_GREEN;
+}
--- hanabi-dataset/src/model/utils/Utils.java
+++ hanabi-dataset/src/model/utils/Utils.java
@@ -1,26 +1,34 @@
 package model.utils;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class Utils {
 
     public static Collection<Double> getDiscardedListFromInt(int discarded){
-        List<Double> res= List.of(0.0,0.0,0.0,0.0,0.0);
+        List<Double> result = new ArrayList<>();
+        //es: 410010 = per il colore 4, un 5 scartato e un 2 scartato
+        String s_discarded = String.valueOf(discarded);
+        result.add((double)s_discarded.charAt(1)); //5 scartati
+        result.add((double)s_discarded.charAt(2)); //4 scartati
+        result.add((double)s_discarded.charAt(3)); //3 scartati
+        result.add((double)s_discarded.charAt(4)); //2 scartati
+        result.add((double)s_discarded.charAt(5)); //1 scartati
 
-        //TODO calcolare le carte scartate a partire da ogni int
-
-
-        return res;
+        return result;
     }
 
-    public static Double[] getDiscardedArrayFromInt(int discarded){
-        Double[] res= {0.0,0.0,0.0,0.0,0.0};
-
-        //TODO calcolare le carte scartate a partire da ogni int
-
-
-        return res;
+    public static double[] getDiscardedArrayFromInt(int discarded){
+        double[] result = new double[5];
+        //es: 410010 = per il colore 4, un 5 scartato e un 2 scartato
+        String s_discarded = String.valueOf(discarded);
+        result[0] = s_discarded.charAt(1);
+        result[1] = s_discarded.charAt(2);
+        result[2] = s_discarded.charAt(3);
+        result[3] = s_discarded.charAt(4);
+        result[4] = s_discarded.charAt(5);
+        return result;
     }
 
 }
