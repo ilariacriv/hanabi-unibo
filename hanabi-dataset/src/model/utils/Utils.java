ceef8c8151f843b160c0c78c52467cb5663e2db9
ilariacriv <58052642+ilariacriv@users.noreply.github.com>
2021-05-22 10:50:14 +0200

    char bugfixed

--- hanabi-dataset/src/model/utils/Utils.java
+++ hanabi-dataset/src/model/utils/Utils.java
@@ -10,11 +10,11 @@ public class Utils {
         List<Double> result = new ArrayList<>();
         //es: 410010 = per il colore 4, un 5 scartato e un 2 scartato
         String s_discarded = String.valueOf(discarded);
-        result.add((double)s_discarded.charAt(1)); //5 scartati
-        result.add((double)s_discarded.charAt(2)); //4 scartati
-        result.add((double)s_discarded.charAt(3)); //3 scartati
-        result.add((double)s_discarded.charAt(4)); //2 scartati
-        result.add((double)s_discarded.charAt(5)); //1 scartati
+        result.add((double)s_discarded.charAt(1)-'0'); //5 scartati
+        result.add((double)s_discarded.charAt(2)-'0'); //4 scartati
+        result.add((double)s_discarded.charAt(3)-'0'); //3 scartati
+        result.add((double)s_discarded.charAt(4)-'0'); //2 scartati
+        result.add((double)s_discarded.charAt(5)-'0'); //1 scartati
 
         return result;
     }
@@ -23,11 +23,11 @@ public class Utils {
         double[] result = new double[5];
         //es: 410010 = per il colore 4, un 5 scartato e un 2 scartato
         String s_discarded = String.valueOf(discarded);
-        result[0] = s_discarded.charAt(1);
-        result[1] = s_discarded.charAt(2);
-        result[2] = s_discarded.charAt(3);
-        result[3] = s_discarded.charAt(4);
-        result[4] = s_discarded.charAt(5);
+        result[0] = s_discarded.charAt(1)-'0';
+        result[1] = s_discarded.charAt(2)-'0';
+        result[2] = s_discarded.charAt(3)-'0';
+        result[3] = s_discarded.charAt(4)-'0';
+        result[4] = s_discarded.charAt(5)-'0';
         return result;
     }
 
